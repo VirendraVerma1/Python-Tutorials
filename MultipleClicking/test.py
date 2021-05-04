7573e0e0818d59df3fa0110c57ab4cf960ad3146
Virendra Verma <virendra@inventics.tech>
2021-05-04 16:39:24 +0530

    update

--- MultipleClicking/test.py
+++ MultipleClicking/test.py
@@ -5,14 +5,14 @@ import os
 import random
 time.sleep(5)
 while(1):
-    num=random.randint(1,4)
-    if(num==1):
-        pyautogui.press('right',presses=1,interval=0.3)
-    if(num==2):
-        pyautogui.press('left',presses=1,interval=0.3)
-    if(num==3):
-        pyautogui.press('up',presses=1,interval=0.3)
-    if(num==4):
+    num=random.randint(1,10)
+    if(num<=5):
         pyautogui.press('down',presses=1,interval=0.3)
+    if(num==6):
+        pyautogui.press('left',presses=1,interval=0.3)
+    if(num==7):
+        pyautogui.press('up',presses=1,interval=0.3)
+    if(num==8):
+        pyautogui.press('right',presses=1,interval=0.3)
     a=random.randint(0,5)
     time.sleep(a)
